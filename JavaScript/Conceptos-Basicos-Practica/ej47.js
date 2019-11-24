292224b4c19df40bcd0eed1b9e920fd4e45d7b52
Romero-Ezequiel <romerorezequiel@gmail.com>
2019-11-24 16:53:13 -0300

    Obtener un array o coleccion desde un string

---
+++ JavaScript/Conceptos-Basicos-Practica/ej47.js
@@ -0,0 +1,13 @@
+/**
+ * Ejercicio 47
+Crear un documento con el nombre ej47.js
+
+Declarar la variable alumnos y asignar el siguiente texto: 
+Matías, Germán, Nicolás, Silvia, Estefi, Patricia, Ramóm
+Asignar a la variable alumnos el resultado de ejecutar el método split sobre la variable alumnos
+Mostrar por consola el contenido de la variable alumnos
+ */
+
+let alumnos = 'Matias, German, Nicolas, Silvia, Estefi, Patricia, Ramón';
+
+console.log(alumnos.split());
