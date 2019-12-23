130508813528ad86034c8daf66bc2f252e02b11a
Romero-Ezequiel <romerorezequiel@gmail.com>
2019-12-22 21:32:08 -0300

    Ejercicios simples de JavaScript

--- JavaScript/Conceptos-Basicos-Practica/ej10.js
+++ JavaScript/Conceptos-Basicos-Practica/ej10.js
@@ -2,7 +2,9 @@
  * Ejercicio 10
 Crear un documento con el nombre ej10.js
 Declarar una variable saludo
-Asignar el valor: ECMAScript rocks!! a la variable saludo, con el tipo de dato string
+Asignar el valor: ECMAScript rocks!! a la variable saludo, 
+con el tipo de dato string
+
 Mostrar el contenido de la variable saludo utilizando console.log()
  */
 
