16a71b56c73dacaa53a972cc29c0457b82c7d690
Romero-Ezequiel <romerorezequiel@gmail.com>
2019-12-22 21:34:22 -0300

    Ejercicios simples de JavaScript

--- JavaScript/Conceptos-Basicos-Practica/ej12.js
+++ JavaScript/Conceptos-Basicos-Practica/ej12.js
@@ -1,10 +1,13 @@
 /**
  * Ejercicio 12
 Crear un documento con el nombre ej12.js
-Declarar una variable nombre con tu nombre
-Declarar una variable saludo con el siguiente mensaje: Bienvenido/a
-Mostrar en consola el mensaje de bienvenida utilizando console.log() y 
-concatenando las variables nombre y saludo para que muestre un mensaje similar a: Bienvenido/a Marta
+
+Declarar una variable nombre con tu nombre. Declarar una variable saludo con 
+el siguiente mensaje: 
+
+Bienvenido/a. Mostrar en consola el mensaje de bienvenida 
+utilizando console.log() y concatenando las variables nombre y 
+saludo para que muestre un mensaje similar a: Bienvenido/a Marta
  */
 
 let nombre='Marta', saludo='Bienvenido/a';
