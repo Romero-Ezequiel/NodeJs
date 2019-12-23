9cb33a51351386d9405ec4ef16fad130806f410b
Romero-Ezequiel <romerorezequiel@gmail.com>
2019-12-22 21:16:44 -0300

    Ejercicios adicionales de javascript

--- JavaScript/Conceptos-Basicos-Practica/Ejercicio1.js
+++ JavaScript/Conceptos-Basicos-Practica/Ejercicio1.js
@@ -1,6 +1,8 @@
-/**Solicita un nombre, la edad y muestra por consola el mensaje "Hola____, tienes ____ años
- * y el año que viene tendrás ____ años. Realiza el ejercicio con prompt (mensaje) y haz
- * uso de los template strings" */
+/**
+Solicita un nombre, la edad y muestra por consola el mensaje 
+"Hola____, tienes ____ años y el año que viene tendrás ____ años. 
+Realiza el ejercicio con prompt (mensaje) y haz  uso de los template strings" 
+*/
 
 let nombre, edad;
 
