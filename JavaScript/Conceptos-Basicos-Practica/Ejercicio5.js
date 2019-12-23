669cd374cda22d83f9411a6060d9a635f045977e
Romero-Ezequiel <romerorezequiel@gmail.com>
2019-12-23 08:14:53 -0300

    Ejercicios adicionales - calcule su factorial

--- JavaScript/Conceptos-Basicos-Practica/Ejercicio5.js
+++ JavaScript/Conceptos-Basicos-Practica/Ejercicio5.js
@@ -1,8 +1,23 @@
 /**
-Escriba un programa que pida un número entero mayor que cero y calcule su factorial. El factorial es el
-resultado de multiplicar ese número por sus anteriores hasta la unidad.
+Escriba un programa que pida un número entero mayor que cero y calcule su factorial. 
+El factorial es el resultado de multiplicar ese número por sus anteriores hasta 
+la unidad.
 
 5! = 5*4*3*2*1 = 120
 
- */
+*/
 
+let numero;
+
+numero = prompt('Ingrese un numero: ');
+
+const  factorial = (numero) =>{
+    if (numero === 0 || numero === 1){
+        return 1;
+    }
+    for(var i=numero-1; i >= 1; i--){
+        numero = numero * i;
+    }
+    return numero
+}
+document.write(factorial(5));
