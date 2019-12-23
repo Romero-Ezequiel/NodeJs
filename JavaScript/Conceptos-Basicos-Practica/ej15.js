b54d0d284a732123938ca7367aaf5708beba1839
Romero-Ezequiel <romerorezequiel@gmail.com>
2019-12-22 21:36:14 -0300

    Ejercicio simple de Js

--- JavaScript/Conceptos-Basicos-Practica/ej15.js
+++ JavaScript/Conceptos-Basicos-Practica/ej15.js
@@ -3,7 +3,8 @@
 Crear un documento con el nombre ej15.js
 Declarar una variable alumno
 Asignar a la variable alumno el nombre del mejor alumno del curso
-Mostrar en consola el siguiente mensaje: El mejor alumno es: y el contenido de la variable alumno
+Mostrar en consola el siguiente mensaje: 
+El mejor alumno es: y el contenido de la variable alumno
 Utilizar interpolación de textos
  */
 
