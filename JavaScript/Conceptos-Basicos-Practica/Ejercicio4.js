0c4ec74581cf0bf557c0338ced7aa1e7a8a0a6ef
Romero-Ezequiel <romerorezequiel@gmail.com>
2019-12-22 21:24:34 -0300

    Otros ejercicios adicionales

--- JavaScript/Conceptos-Basicos-Practica/Ejercicio4.js
+++ JavaScript/Conceptos-Basicos-Practica/Ejercicio4.js
@@ -1,5 +1,11 @@
 /**
-Escribe un programa que pida un número entero mayor que 1 y que escriba si el número primo o no. Un
-número primo es aquel que solo es divisible por si mismo y la unidad
+Escribe un programa que pida un número entero mayor que 1 y
+que escriba si el número primo o no. Un número primo es aquel que 
+solo es divisible por si mismo y la unidad
 */
 
+let numero, j=2 ;
+
+numero = prompt('Ingrese un numero: ');
+
+(numero >= 1) ? console.log() : console.log('Intente de nuevo');
