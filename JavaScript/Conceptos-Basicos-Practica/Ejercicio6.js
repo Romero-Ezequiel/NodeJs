1438c7fb68b313ee5e87a8f59d05976fd0b221cf
Romero-Ezequiel <romerorezequiel@gmail.com>
2019-12-26 19:23:57 -0300

    Ejercicios adicionales

--- JavaScript/Conceptos-Basicos-Practica/Ejercicio6.js
+++ JavaScript/Conceptos-Basicos-Practica/Ejercicio6.js
@@ -1,6 +1,15 @@
 /**
- * Escribe un programa que permita ir introduciendo una serie indeterminada de números mientras su suma
- * no supere 50. Cuando esto ocurra, se debe mostrar el total acumulado y el contador de cuantos números
+ * Escribe un programa que permita ir introduciendo una serie indeterminada de
+ * números mientras su suma no supere 50. Cuando esto ocurra, se debe mostrar 
+ * el total acumulado y el contador de cuantos números
  * se han introducido
  */
 
+let suma=0, contador;
+
+while(suma <= 50){
+    suma += parseInt(prompt('Ingrese un numero para añadir a la suma'));
+    contador++;
+}
+document.write('La suma total es: '+ suma);
+document.write('El total de numeros es '+ contador);
