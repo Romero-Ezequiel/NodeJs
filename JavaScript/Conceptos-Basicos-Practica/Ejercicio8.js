cd0550ecacdb86617113cfda8f20f85a0dc70e79
Romero-Ezequiel <romerorezequiel@gmail.com>
2019-12-26 22:49:56 -0300

    Ejercicios adicionales

--- JavaScript/Conceptos-Basicos-Practica/Ejercicio8.js
+++ JavaScript/Conceptos-Basicos-Practica/Ejercicio8.js
@@ -1,6 +1,7 @@
 /**
- * Dado un array de letras, solicita un número de DNI y calcula que letra le corresponde. El número no puede 
- * ser negativo ni tener más de 8 digitos. La posicion de la letra es el resultado del modulo del número
+ * Dado un array de letras, solicita un número de DNI y calcula que letra 
+ * le corresponde. El número no puede ser negativo ni tener más de 8 digitos. 
+ * La posicion de la letra es el resultado del modulo del número 
  * introducido entre 23.
 
 const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y','F','P','D','X','B','N',
@@ -8,3 +9,10 @@ const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y','F','P','D','X','B','N',
  
  */
 
+const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y','F','P','D','X','B','N', 'J','Z','S','Q','V','H','L','C','K','E','T'];
+
+const dni = prompt('Introduce tu DNI');
+if(dni.length==8 && parseInt(dni)>0){
+    let letra = dni%23;
+    console.log(`Tu DNI completo es  ${dni}${letras[letra]}`);
+}
