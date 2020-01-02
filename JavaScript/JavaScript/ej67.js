ce333a5e689d17ab063351adb51f8211eeecfaa6
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-02 17:09:42 -0300

    Ejercicio adicional de while

---
+++ JavaScript/JavaScript/ej67.js
@@ -0,0 +1,13 @@
+/**
+ * Ejercicio 67
+Crear un documento con el nombre ej67.js
+Mostrar en consola los números del 100 al 0 utilizando la estructura while
+ */
+
+let i=100;
+
+while(i >= 0){
+    console.log(i);
+    i--;
+    
+}
