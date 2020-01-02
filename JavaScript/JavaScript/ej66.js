603eb5722d25c58639e0d3ce88f693e7e508d515
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-02 17:04:01 -0300

    Ejercicio de repaso de while

---
+++ JavaScript/JavaScript/ej66.js
@@ -0,0 +1,12 @@
+/**
+ * Ejercicio 66
+Crear un documento con el nombre ej66.js
+Mostrar en consola los números del 0 al 10 utilizando la estructura while
+ */
+
+i=0;
+while(i <= 10){
+    console.log(i);
+    i++;
+    
+}
