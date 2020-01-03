b2bf838b567ca083e9baa4e78ba76094e525bc5a
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-03 13:23:06 -0300

    Ejercicio adicional de estructura de control

---
+++ JavaScript/JavaScript/ej71.js
@@ -0,0 +1,14 @@
+/**
+ * Ejercicio 71
+Crear un documento con el nombre ej71.js
+Mostrar en consola la tabla de multiplicar del 9 (de 1 a 10) utilizando 
+la estructura while
+ */
+
+let i=0;
+
+while(i <= 10){
+
+    console.log(i*(9));
+    i++;
+}
