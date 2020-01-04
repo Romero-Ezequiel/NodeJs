7c4a5656f809d98893e978fa5d5c3415a364bfe3
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-04 15:24:07 -0300

    Ejercicios adicionales

---
+++ JavaScript/JavaScript/ej86.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * Ejercicio 86
+Crear un documento con el nombre ej86.js
+Copiar y pegar el código del ejercicio ej76.js
+Refactorizar el código utilizando la estructura for en lugar de do/while
+ */
+
+
+for(let i=0; i <= 10; i++){
+    console.log(i);
+}
