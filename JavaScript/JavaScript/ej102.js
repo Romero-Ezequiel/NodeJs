f2fc338211c8cace3bf1f3ccd5a6af6c9beb0c70
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-04 19:39:21 -0300

    Ejercicio adicional

---
+++ JavaScript/JavaScript/ej102.js
@@ -0,0 +1,20 @@
+/**
+ * Ejercicio 102
+Crear un documento con el nombre ej102.js
+Declarar una variable mostrarNombre y asignar una función
+La función mostrarNombre tiene que mostrar tu nombre en consola 
+con el siguiente formato:
+  ===========
+  = Nicolás =
+  ===========
+Ejecutar esta función 2 veces
+ */
+
+const mostrarNombre = (nombre) =>{
+  console.log(`============\n
+= ${nombre} =\n
+============`)
+
+};
+
+mostrarNombre('Ezequiel');
