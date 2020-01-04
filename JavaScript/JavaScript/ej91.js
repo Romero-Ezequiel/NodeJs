6ec6f4265a09a035a5d88a7bb746a90e48ee75af
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-04 17:15:11 -0300

    Ejercicios adicional

---
+++ JavaScript/JavaScript/ej91.js
@@ -0,0 +1,13 @@
+/**
+ * Ejercicio 91
+Crear un documento con el nombre ej91.js
+Copiar y pegar el código del ejercicio ej81.js
+Refactorizar el código utilizando la estructura for en lugar de do/while
+ */
+
+let i=0;
+
+for(i; i <= 10; i++){
+    console.log(i*(9));
+}
+
