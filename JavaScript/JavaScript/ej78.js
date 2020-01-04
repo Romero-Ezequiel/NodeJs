697f6749f195486d472f2f094920550137db6ed2
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-04 15:03:32 -0300

    Ejercicios adicionales

---
+++ JavaScript/JavaScript/ej78.js
@@ -0,0 +1,15 @@
+/**
+ * Ejercicio 78
+Crear un documento con el nombre ej78.js
+Copiar y pegar el código del ejercicio ej68.js
+Refactorizar el código utilizando la estructura do/while en lugar de while
+ */
+
+
+let i = 0;
+
+do{
+    console.log(i);
+    i++;
+}
+while(i <= 1000)
