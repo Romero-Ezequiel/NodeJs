4516d3eb6570189d534adcefd718e0c86112d630
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-04 14:58:23 -0300

    Ejercicios adicionales

---
+++ JavaScript/JavaScript/ej76.js
@@ -0,0 +1,12 @@
+/**
+ * Ejercicio 76
+Crear un documento con el nombre ej76.js
+Copiar y pegar el código del ejercicio ej66.js
+Refactorizar el código utilizando la estructura do/while en lugar de while
+ */
+
+i=0;
+do{
+    console.log(i);
+    i++;
+}while(i <= 10)
