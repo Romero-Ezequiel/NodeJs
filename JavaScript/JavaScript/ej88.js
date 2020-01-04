1f3512724c696c6c5df8adce478c26dafb9e4ff5
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-04 17:09:09 -0300

    Ejercicio adicionales

---
+++ JavaScript/JavaScript/ej88.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * Ejercicio 88
+Crear un documento con el nombre ej88.js
+Copiar y pegar el código del ejercicio ej78.js
+Refactorizar el código utilizando la estructura for en lugar de do/while
+ */
+
+let i = 0;
+for(i; i <= 1000; i++){
+    console.log(i);
+}
