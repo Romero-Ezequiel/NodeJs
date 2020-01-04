0d8483557bea917ed758a40f5318f45ac272ab4f
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-04 17:06:41 -0300

    Ejercicio adicional

---
+++ JavaScript/JavaScript/ej87.js
@@ -0,0 +1,12 @@
+/**
+ * Ejercicio 87
+Crear un documento con el nombre ej87.js
+Copiar y pegar el código del ejercicio ej77.js
+Refactorizar el código utilizando la estructura for en lugar de do/while
+ */
+
+
+
+for(let i = 100; i >=0 ; i--){
+    console.log(i);
+}
