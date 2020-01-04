1a4ba52d79313b3156786c977a0cb3b8f432dbf5
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-04 19:48:07 -0300

    Ejercicio adicional

---
+++ JavaScript/JavaScript/ej105.js
@@ -0,0 +1,13 @@
+/**
+ * Ejercicio 105
+Crear un documento con el nombre ej105.js
+Crear una función que se llame significadoDeLaVida y retorna el número 42
+Mostrar en consola el siguiente mensaje: 
+El significado de la vida es ${significadoDeLaVida}
+ */
+
+const significadoDeLaVida = () =>{
+    return 42;
+}
+
+console.log(`El significado de la vida es ${significadoDeLaVida()}`)
