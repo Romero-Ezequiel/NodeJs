9dddda8ec12b7ea52bc9b88f236f47ce274139e1
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-10 19:59:42 -0300

    Consigna del ejercicio

--- NodeJs/Ejercicio2/ej2.js
+++ NodeJs/Ejercicio2/ej2.js
@@ -5,6 +5,7 @@ Inicializar un proyecto de Node.js utilizando NPM dentro de la carpeta ej2
 El nombre del proyecto tiene que ser Ejercicio 2
 Crear un archivo index.js
 Instalar el módulo Express como dependencia de nuestro proyecto
-Configurar el proyecto para que al correr npm start corra el código del
+Configurar el proyecto para que al correr npm start
+corra el código del
 archivo index.js. Mostrar en consola el objeto express
  */
