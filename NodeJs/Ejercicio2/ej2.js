32313d53e09e3dea8e9f2177041f613db0987f34
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-10 19:40:34 -0300

    Ejercicios adicionales

---
+++ NodeJs/Ejercicio2/ej2.js
@@ -0,0 +1,10 @@
+/**
+ * Ejercicio 2
+Crear una carpeta con el nombre ej2
+Inicializar un proyecto de Node.js utilizando NPM dentro de la carpeta ej2
+El nombre del proyecto tiene que ser Ejercicio 2
+Crear un archivo index.js
+Instalar el módulo Express como dependencia de nuestro proyecto
+Configurar el proyecto para que al correr npm start corra el código del
+archivo index.js. Mostrar en consola el objeto express
+ */
