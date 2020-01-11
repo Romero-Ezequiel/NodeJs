5508e46541e029ec33c27599278b3fe2877f8033
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-11 16:34:01 -0300

    Enunciado del ejercicio

---
+++ NodeJs/Ejercicio7/ej7.js
@@ -0,0 +1,15 @@
+/**
+ * Ejercicio 7
+Crear una carpeta con el nombre ej7
+Crear un archivo con el nombre index.js
+Crear un archivo con el nombre saludador.js
+Requerir el módulo saludador
+Utilizar la función saludar exportada
+Pasarle los 3 parámetros desde este archivo (nombre, apellido y callback)
+saludador.js
+Copiar y pegar la función saludar del ejercicio anterior (ej6) en el archivo 
+saludador.js
+Exportar la función saludar
+
+Correr el proyecto con npm start (index.js)
+ */
