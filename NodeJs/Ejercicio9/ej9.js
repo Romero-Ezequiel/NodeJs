976a67b762daa6996f2c70b2c191d8b8aa3e1056
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-11 17:52:17 -0300

    Enunciado del ejercicio

---
+++ NodeJs/Ejercicio9/ej9.js
@@ -0,0 +1,15 @@
+/**
+Ejercicio 9
+
+Crear un archivo con el nombre ej9.js
+Levantar un servidor utilizando Express, el servidor tiene que responder con 
+el siguiente texto: 'Bienvenidos a Node.js Server Side'
+
+Al levantar el servidor tiene que mostrar un mensaje que diga: 
+`Servidor corriendo en el puerto ${puerto}`. En caso de haber un error al levantar
+el servidor tiene que mostrar el siguiente mensaje:
+
+`No se pudo levantar el servidor en el puerto ${puerto}`
+
+*/
+
