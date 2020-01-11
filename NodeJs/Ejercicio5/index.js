6740ee42d1ce226a9d2656d0a5dd7d4396074e24
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-10 23:02:41 -0300

    Archivo index.js

---
+++ NodeJs/Ejercicio5/index.js
@@ -0,0 +1,11 @@
+const Logger = require('logplease');
+
+let numero = require('./numeros.js');
+
+console.log("El numero 2 es par : " + numero(2));
+console.log("El numero 3 es par : " + numero(3));
+console.log("El numero 101 es par : " + numero(101));
+console.log("El numero 201 es par : " +numero(201));
+console.log("El numero 202 es par : " +numero(202));
+console.log("El numero 100 es par : " +numero(100));
+
