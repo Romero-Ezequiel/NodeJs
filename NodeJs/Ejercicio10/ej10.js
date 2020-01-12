fd0782bfd375470b89b92c6d45927c6fc3301c16
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-12 12:27:26 -0300

    Enunciado del ejercicio

---
+++ NodeJs/Ejercicio10/ej10.js
@@ -0,0 +1,24 @@
+/**
+ * Ejercicio 10
+Crear un archivo con el nombre ej10.js
+Levantar un servidor de Express
+Manejar las siguientes rutas:
+Ruta: Raiz del sitio ,Metodo: get, Acción: Mostrar un mensaje de bienvenida
+Ruta: Productos, Metodo: get, Acción: Mostrar un mensaje que diga: listado de
+productos
+Ruta: Productos, Metodo: post, Acción: Mostrar un mensaje que diga: crear 
+un producto
+Ruta: Productos, Metodo: put, Acción: Mostrar un mensaje que diga: actualizar
+un producto
+Ruta: Productos, Metodo: delete, Acción: Mostrar un mensaje que diga: borrar 
+un producto
+Ruta: Usuarios, Metodo: get, Acción: Mostrar un mensaje que diga: listado de
+usuarios
+Ruta: Usuarios, Metodo: post, Acción: Mostrar un mensaje que diga: crear un usuario
+Ruta: Usuarios, Metodo: put, Acción: Mostrar un mensaje que diga: actualizar 
+un usuario
+Ruta: Usuarios, Metodo: delete, Acción: Mostrar un mensaje que diga: borrar 
+un usuario
+Crear un método que maneje todos los verbos de HTTP para la pagina
+Utilizar Postman para probar todos los llamados
+ */
