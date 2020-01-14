466a3ebf46d566776c06f520244b27bd7820c7a8
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-13 23:25:57 -0300

    Enunciado del ejercicio

---
+++ NodeJs/Ejercicio12/ej12.js
@@ -0,0 +1,17 @@
+/**
+ * Ejercicio 12
+Crear un archivo con el nombre ej12.js
+Levantar un servidor de Express
+Al llamar a localhost:3000/api/products se debe mostrar el siguiente JSON:
+{
+  descripcion: 'Productos',
+  items: [
+    { nombre: 'taza de Star Wars' , precio: 300},
+    { nombre: 'FIFA 22 PS4' , precio: 1000},
+    { nombre: 'Remera superheore' , precio: 100},
+    { nombre: 'Bincha de Piñon fijo' , precio: 200},
+    { nombre: 'Grande de Muzza' , precio: 120},
+    { nombre: 'Botella de Fernet por 1 litro' , precio: 220}
+  ]
+}
+ */
