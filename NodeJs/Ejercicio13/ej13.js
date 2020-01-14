63b0d78766ddb3ce9d2f36fa4a55edb88e8eeac6
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-01-14 00:03:14 -0300

    Enunciado del ejercicio

---
+++ NodeJs/Ejercicio13/ej13.js
@@ -0,0 +1,12 @@
+/**
+ * Ejercicio 13
+Crear un archivo con el nombre ej13.js
+Levantar un servidor de Express
+Crear un módulo para manejar las rutas de nuestra home (get y post)
+Crear un módulo para manejar las rutas de nuestros productos (get, post, delete, put)
+Cada ruta sólo tiene que enviar un mensaje diciendo que página es y 
+que método utiliza
+res.send('Pagina: home, método: post')
+Montar las rutas de home en '/'
+Montar las rutas de prodcutos en '/productos'
+ */
